c398997efec4d8516f77fc2ee8e92385abcfdb50
enisgjini <gjinienis148@gmail.com>
2022-10-07 21:14:04 +0200

    #

---
+++ src/components/Profile.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import JSONPretty from "react-json-pretty";
+import "../App.css";
+
+const Profile = () => {
+  const { user, isAuthenticated } = useAuth0();
+  return (
+    isAuthenticated && (
+      <div className="card">
+        <img src={user.picture} alt={user.name} />
+        <h2>Name : {user.name}</h2>
+        <table id="customers">
+          <tr>
+            <td>
+              <i class="fa-regular fa-envelope"></i> Email
+            </td>
+            <td>{user.email}</td>
+          </tr>
+          <tr>
+            <td>
+              <i class="fa-solid fa-signature"></i> Nickname
+            </td>
+            <td>{user.nickname}</td>
+          </tr>
+          <tr>
+            <td>
+              <i class="fa-solid fa-location-dot"></i> Locale
+            </td>
+            <td>{user.locale}</td>
+          </tr>
+          <tr>
+            <td>
+              <i class="fa-solid fa-arrows-rotate"></i> Updated at
+            </td>
+            <td>{user.updated_at}</td>
+          </tr>
+          <tr>
+            <td>
+              <i class="fa-solid fa-hashtag"></i> Sub
+            </td>
+            <td>{user.sub}</td>
+          </tr>
+        </table>
+
+        {/* <JSONPretty data={user} /> */}
+      </div>
+    )
+  );
+};
+
+export default Profile;
